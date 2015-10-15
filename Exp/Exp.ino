0a4ef7b5552f48db6842bdd1430cf35d59583879
Troels Krøgh <troels.kroegh@gmail.com>
2015-10-15 23:33:40 +0200

    ....

--- Exp/Exp.ino
+++ Exp/Exp.ino
@@ -10,6 +10,9 @@
 #include <EEPROM.h>
 #include "Addr.h"
 
+// Sass' payload format ?
+#include "sass_ask.h"
+
 // General headers
 #include <inttypes.h>
 
@@ -29,6 +32,9 @@ uint8_t addr = 0;
 Timer t;
 RH_ASK rh;
 
+uint8_t RED_PIN_ID = 0;
+uint8_t GREEN_PIN_ID = 0;
+
 int listenForNetwork() {
 #ifdef DEBUG
   Serial.print("Started listening for network. Max time: ");
@@ -40,8 +46,8 @@ int listenForNetwork() {
   uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
   uint8_t buflen = sizeof(buf);
 
-  int recivedPackages = 0;  
-  t.oscillate(GREEN_PIN, 100, LOW, MAX_WAIT_FOR_NETWORK_TIME_MS / 100);
+  int recivedPackages = 0;
+  GREEN_PIN_ID = t.oscillate(GREEN_PIN, 100, LOW, MAX_WAIT_FOR_NETWORK_TIME_MS / 100);
 
   while (millis() - startTime < MAX_WAIT_FOR_NETWORK_TIME_MS) {
     // Call recv as often as posible
@@ -55,16 +61,26 @@ int listenForNetwork() {
     t.update();
   }
 
-  t.stop(GREEN_PIN);
+  t.stop(GREEN_PIN_ID);
 
   return recivedPackages;
 }
 
 void ProcessPackage(const uint8_t* buf, uint8_t len) {
-  #ifdef DEBUG
-  rh.printBuffer("Got: ", buf, len);
-  #endif
-  // TODO
+  if(len < 4) {
+    return;
+  }
+  payload_type * tmp = (payload_type *) malloc(sizeof(payload_type) + 1);
+  memcpy(&tmp, &buf, len);
+  tmp->msga[len - 4] = '\0';
+  // msg isn't null terminated? 
+  
+#ifdef DEBUG
+  char stringbuffer[64];
+  makePayloadString(*tmp, stringbuffer);
+  Serial.println(stringbuffer);
+  //rh.printBuffer("Got: ", buf, len);
+#endif
 }
 
 void setup() {
@@ -77,8 +93,8 @@ void setup() {
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
 
-  t.oscillate(RED_PIN, 1000, LOW);
-  
+  RED_PIN_ID = t.oscillate(RED_PIN, 1000, LOW);
+
 
   Addr a;
   addr = a.get();
@@ -91,17 +107,28 @@ void setup() {
   }
 #endif
 
-  if(listenForNetwork() > 0) {
+  t.stop(RED_PIN_ID);
+  t.stop(GREEN_PIN_ID);
+  if (listenForNetwork() > 0) {
     // Network found!
-    #ifdef DEBUG
+#ifdef DEBUG
     Serial.println("Network found, waiting for input on button before attempting to join");
-    #endif
-    t.stop(RED_PIN);
-    t.stop(GREEN_PIN);
-    t.oscillate(GREEN_PIN, 250, LOW);
-    while(digitalRead(BUTTON_PIN) == HIGH);
+#endif
+    GREEN_PIN_ID = t.oscillate(GREEN_PIN, 250, HIGH);
+    while (digitalRead(BUTTON_PIN) == HIGH) {
+      // TODO LISTEN TO NETWORK WHILE WAITING ?
+      t.update();
+    }
   } else {
     // No network found. :(
+#ifdef DEBUG
+    Serial.println("No network found. Waiting for input on button before attempting to start one.");
+#endif
+    RED_PIN_ID = t.oscillate(RED_PIN, 250, HIGH);
+    while (digitalRead(BUTTON_PIN) == HIGH) {
+      t.update();
+    }
+    
   }
 
   Serial.println("----- END OF SETUP -----");
